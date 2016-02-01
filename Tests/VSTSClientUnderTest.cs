232d0efdd6f7f7dd9aa490ee868367822d286ef5
Julien Stroheker <julienstroheker@gmail.com>
2016-01-31 17:19:59 -0800

    Add structure for tests

---
+++ Tests/VSTSClientUnderTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hackathon_GetStarted;
+
+namespace Tests
+{
+    [TestClass]
+    class VSTSClientUnderTest
+    {
+        [TestMethod]
+        public void GetVSTSClient()
+        {
+            
+        }
+    }
+}
